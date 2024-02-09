74394570d27a0493e871bd227769d0df16ac70c3
Radmilo Borovina <radmiloborovina99@gmail.com>
2024-02-09 18:02:53 +0100

    recaptcha verification added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const memoryStorage = multer.memoryStorage();
 const nodemailer = require('nodemailer');
 const nodemailerMailjet = require('nodemailer-mailjet-transport');
 const rateLimit = require('express-rate-limit');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,13 +23,32 @@ const upload = multer({
   }
 });
 
+const verifyRecaptcha = async (token) => {
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+
+  try {
+    const response = await axios.post(url);
+    return response.data.success;
+  } catch (error) {
+    console.error('reCAPTCHA verification failed:', error.response.data);
+    return false;
+  }
+};
+
 app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.post('/submit-form', upload.array('photos', 5), async (req, res) => {
-  const {name, email} = req.body;
+  const {name, email, token} = req.body;
   const photos = req.files;
+
+  const isRecaptchaValid = await verifyRecaptcha(token);
+
+  if (!isRecaptchaValid) {
+    return res.status(400).send('reCAPTCHA verification failed');
+  }
 
   const transporter = nodemailer.createTransport(nodemailerMailjet({
     auth: {
@@ -45,7 +65,7 @@ app.post('/submit-form', upload.array('photos', 5), async (req, res) => {
             Ime: ${name}
             Email: ${email}
         `,
-    attachments:[]
+    attachments: []
   };
 
   photos.forEach((photo, index) => {
